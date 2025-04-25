57f33039dbf2b4d7fd098f776781ab7d90b7b2bb
qazalnaderi <ghazalnaderi4@gmail.com>
2025-04-25 23:54:34 +0330

    get website category api created

--- backend/services/core/app/api/v1/endpoints/websites_routes.py
+++ backend/services/core/app/api/v1/endpoints/websites_routes.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from app.services.website_main_service import WebsiteMainService
-from app.domain.schemas.website_schema import (WebsiteCreateSchema, WebsiteResponseSchema, WebsiteCategoryCreateSchema, WebsiteCategoryResponseSchema)
+from app.domain.schemas.website_schema import (WebsiteCreateSchema, WebsiteResponseSchema, WebsiteCategoryCreateSchema,
+WebsiteCategoryResponseSchema, WebsiteCategoryResponseSchema, WebsiteSubcategoryResponseSchema, WebsiteSubcategoryCreateSchema)
 from app.services.auth_services.auth_service import get_current_user
 from app.domain.schemas.token_schema import TokenDataSchema
 from loguru import logger
-from typing import Annotated
+from typing import Annotated, List
 from uuid import UUID
 
 website_router = APIRouter()
@@ -19,6 +20,16 @@ async def create_website(
     
     return await website_service.create_website(current_user.user_id, website_data)
 
+@website_router.get("/get_website/{website_id}", response_model=WebsiteResponseSchema, status_code=status.HTTP_200_OK)
+async def get_website(
+    website_id: UUID,   
+    website_service: Annotated[WebsiteMainService, Depends()]
+):
+    logger.info(f"Requesting website details with website_id: {website_id}.")
+    
+    return await website_service.get_website_by_id(website_id)
+
+
 @website_router.post("/create_website_category", response_model=WebsiteCategoryResponseSchema, status_code=status.HTTP_201_CREATED)
 async def create_website_category(
     website_category_data: WebsiteCategoryCreateSchema,
@@ -30,13 +41,22 @@ async def create_website_category(
     return created_category
 
 
-@website_router.get("/get_website/{website_id}", response_model=WebsiteResponseSchema, status_code=status.HTTP_200_OK)
-async def get_website(
-    website_id: UUID,   
+@website_router.get("/get_website_categories/{website_id}", response_model=List[WebsiteCategoryResponseSchema], status_code=status.HTTP_200_OK)
+async def get_website_categories(
+    website_id: UUID,
+    website_service: Annotated[WebsiteMainService, Depends()] 
+):
+    logger.info(f"Requesting website categories for website id: {website_id}")
+    categories = await website_service.get_website_categories_by_website_id(website_id)
+    return categories
+
+
+@website_router.post("/create_website_subcategory", response_model=WebsiteSubcategoryResponseSchema, status_code=status.HTTP_201_CREATED)
+async def create_website_subcategory(
+    subcategory_data: WebsiteSubcategoryCreateSchema,
+    current_user: Annotated[TokenDataSchema, Depends(get_current_user)],
     website_service: Annotated[WebsiteMainService, Depends()]
 ):
-    logger.info(f"Requesting website details with website_id: {website_id}.")
-    
-    return await website_service.get_website_by_id(website_id)
-
-
+    logger.info(f"User {current_user.user_id} is creating a website subcategory with data: {subcategory_data.dict()}")
+    created_subcategory = await website_service.create_website_subcategory(subcategory_data)
+    return created_subcategory
