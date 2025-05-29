34375e6bb920ae559b457bb955a1403e36c5ea3a
qazalnaderi <ghazalnaderi4@gmail.com>
2025-05-29 15:20:07 +0330

    deactive expired plan crud created

---
+++ backend/services/core/app/infrastructure/repositories/plan_repository.py
@@ -0,0 +1,23 @@
+from typing import Annotated, Dict
+from loguru import logger
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from  app.core.postgres_db.database import get_db
+from  app.domain.models.website_model import WebsitePlan
+from datetime import datetime
+
+
+class PlanRepository:
+    def __init__(self, db: Annotated[Session, Depends(get_db)]):
+        self.db = db
+
+    def deactivate_expired_plans(self):
+        now = datetime.utcnow()
+        expired_plans = self.db.query(WebsitePlan).filter(
+            WebsitePlan.expires_at <= now,
+            WebsitePlan.is_active == True
+        ).all()
+        for plan in expired_plans:
+            plan.is_active = False
+        self.db.commit()
+        return expired_plans
