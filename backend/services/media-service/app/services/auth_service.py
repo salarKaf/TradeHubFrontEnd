30e2b4afde807d1c87e5fa983a80f41cec606091
qazalnaderi <ghazalnaderi4@gmail.com>
2025-07-19 22:24:51 +0330

    bug fixed

--- backend/services/media-service/app/services/auth_service.py
+++ backend/services/media-service/app/services/auth_service.py
@@ -1,31 +1,26 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from typing import Annotated
 from loguru import logger
-import jwt
-from fastapi import Depends, HTTPException, status
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from app.domain.models.user_model import User
 from app.infrastructure.clients.iam_client import IAMClient
+from app.domain.schemas.token_schema import TokenDataSchema
+from app.core.config.config import get_settings
 
-http_bearer = HTTPBearer() 
+config = get_settings()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"http://iam.localhost/api/v1/users/login")
+
 
 async def get_current_user(
-    client: Annotated[IAMClient, Depends()],
-    token: HTTPAuthorizationCredentials = Depends(http_bearer), 
+        token: Annotated[str, Depends(oauth2_scheme)],
+        client: Annotated[IAMClient, Depends()],
+) -> TokenDataSchema:
+    if not token:
+        logger.error("No token provided")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Unauthorized",
+        )
 
-) -> User:
-        credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-        if not token:
-            logger.error("No token provided")
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Unauthorized",
-            )
-
-        logger.info(f"Validating token {token}")
-        return await client.validate_token(token)
-
-
+    logger.info(f"Validating token {token}")
+    return await client.validate_token(token)
