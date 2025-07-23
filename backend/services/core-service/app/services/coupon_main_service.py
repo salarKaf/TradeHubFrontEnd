12551192220436eb257f9f1fb3216ccee7ee0b6c
qazalnaderi <ghazalnaderi4@gmail.com>
2025-07-23 22:14:01 +0330

    bug fixed

--- backend/services/core-service/app/services/coupon_main_service.py
+++ backend/services/core-service/app/services/coupon_main_service.py
@@ -1,9 +1,9 @@
 from fastapi import HTTPException, Depends
 from app.domain.schemas.coupon_schema import CouponCreateSchema, CouponResponseSchema
 from app.domain.models.website_model import Coupon
-from app.infrastructure.repositories.coupon_repository import CouponRepository
+from app.services.coupon_service import  CouponService
 from uuid import UUID
-from app.infrastructure.repositories.website_repository import WebsiteRepository
+from app.services.website_service import WebsiteService
 from typing import Annotated, List
 from loguru import logger
 
@@ -11,60 +11,40 @@ from loguru import logger
 class CouponMainService:
     def __init__(
         self,
-        coupon_repository: Annotated[CouponRepository, Depends()],
-        website_repository: Annotated[WebsiteRepository, Depends()],
+        service: Annotated[CouponService, Depends()],
+        website_service: Annotated[WebsiteService, Depends()],
     ):
-        self.coupon_repository = coupon_repository
-        self.website_repository = website_repository
+        self.service = service
+        self.website_service = website_service
 
-    async def create_coupon(
-        self,
-        coupon_data: CouponCreateSchema
-    ) -> CouponResponseSchema:
+    async def create_coupon(self, coupon_data: CouponCreateSchema) -> CouponResponseSchema:
+        
         logger.info(f"Starting create_coupon with data: {coupon_data.dict()}")
 
-        website = self.website_repository.get_website_by_id(coupon_data.website_id)
+        website = await self.website_service.get_website_by_id(coupon_data.website_id)
         if not website:
             raise HTTPException(status_code=404, detail="Website not found")
 
-        new_coupon = Coupon(
-            website_id=coupon_data.website_id,
-            code=coupon_data.code,
-            discount_amount=coupon_data.discount_amount,
-            expiration_date=coupon_data.expiration_date,
-            usage_limit=coupon_data.usage_limit,
-        )
+        created = await self.service.create_coupon(coupon_data)
+        return CouponResponseSchema.from_orm(created)
 
-        created = self.coupon_repository.create_coupon(new_coupon)
-        logger.info(f"Coupon created: {created.coupon_id}")
+    async def get_coupon_by_code(self, coupon_code:str) ->CouponResponseSchema:
+        coupon = await  self.service.get_coupon_by_code(coupon_code)
+        return CouponResponseSchema.from_orm(coupon)
 
-        return CouponResponseSchema(
-            id=created.coupon_id,
-            website_id=created.website_id,
-            code=created.code,
-            discount_amount=created.discount_amount,
-            expiration_date=created.expiration_date,
-            usage_limit=created.usage_limit,
-            times_used=created.times_used,
-            created_at=created.created_at,
-        )
-
-    async def get_coupon_by_code(self, coupon_code:str) ->Coupon:
-        return self.coupon_repository.get_coupon_by_code(coupon_code)
-    
 
     async def get_coupons_by_website_id(self, website_id: UUID) -> List[CouponResponseSchema]:
-        website = self.website_repository.get_website_by_id(website_id)
+        website = await self.website_service.get_website_by_id(website_id)
         if not website:
             raise HTTPException(status_code=404, detail="Website not found")
 
-        coupons = self.coupon_repository.get_coupons_by_website_id(website_id)
+        coupons = await self.service.get_coupons_by_website_id(website_id)
         return [CouponResponseSchema.from_orm(c) for c in coupons]
 
 
     async def delete_coupon(self, coupon_id: UUID) -> None:
-        coupon = self.coupon_repository.get_coupon_by_id(coupon_id)
+        coupon = await self.service.get_coupon_by_id(coupon_id)
         if not coupon:
             raise HTTPException(status_code=404, detail="Coupon not found")
 
-        self.coupon_repository.delete_coupon(coupon_id)
+        return await self.service.delete_coupon(coupon_id)
