b11a557aa8a7c752418c5684d744af5dfa8cd6b1
qazalnaderi <ghazalnaderi4@gmail.com>
2025-07-25 23:14:39 +0330

    bug fixed

--- backend/services/core-service/app/services/slug_smain_service.py
+++ backend/services/core-service/app/services/slug_smain_service.py
@@ -9,7 +9,7 @@ class SlugMainService:
         self.slug_service = slug_service
 
     async def create_slug(self, slug: str, website_id: UUID) -> bool:
-        slug = await self.slug_service.get_website_id_by_slug(slug)
+        slug = await self.slug_service.get_slug(slug)
         if slug:
             raise HTTPException(status_code=403, detail="Slug already exists.")
         return await self.slug_service.create_slug(slug, website_id)
