f6817251e8d69229f98cdd316d0123e618002161
qazalnaderi <ghazalnaderi4@gmail.com>
2025-07-26 22:57:18 +0330

    authorization fixed

--- backend/services/core-service/app/services/auth_services/buyer_auth_service.py
+++ backend/services/core-service/app/services/auth_services/buyer_auth_service.py
@@ -1,26 +1,50 @@
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta, timezone
 from typing import Annotated
 from loguru import logger
-from app.infrastructure.clients.iam_client import IAMClient
-from app.domain.schemas.token_schema import TokenDataSchema
-from app.core.configs.config import get_settings
-
-config = get_settings()
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"http://iam.localhost/api/v1/buyers/login", scheme_name="BuyerOAuth2")
+import jwt
+from fastapi import Depends, HTTPException, status
+from  app.services.buyer_service import BuyerService
+from app.domain.schemas.token_schema import TokenDataSchema, TokenSchema
+from app.domain.schemas.admin_schema import AdminLoginSchema
+from app.domain.schemas.buyer_schema import BuyerLoginSchema
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from app.domain.models.buyer_model import Buyer
+http_bearer = HTTPBearer() 
 
 
 async def get_current_buyer(
-        token: Annotated[str, Depends(oauth2_scheme)],
-        client: Annotated[IAMClient, Depends()],
-) -> TokenDataSchema:
-    if not token:
-        logger.error("No token provided")
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Unauthorized",
-        )
+    buyer_service: Annotated[BuyerService, Depends()],
+    token: HTTPAuthorizationCredentials = Depends(http_bearer),  
 
-    logger.info(f"Validating token {token}")
-    return await client.validate_buyer_token(token)
+) -> Buyer:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    logger.info(f"Validating token for Buyer {token}")
+    
+    try:
+        payload = jwt.decode(
+            token.credentials,
+            buyer_service.config.JWT_SECRET_KEY,
+            algorithms=[buyer_service.config.JWT_ALGORITHM],
+        )
+        buyer_id: str = payload.get("sub")
+        role: str = payload.get("role")  
+
+        if role != "buyer":
+            raise credentials_exception
+
+        buyer = await buyer_service.get_buyer_by_id(buyer_id)
+        if not buyer:
+            logger.error("buyer not found")
+            raise credentials_exception
+
+    except jwt.PyJWTError:
+        logger.error("Error decoding token")
+        raise credentials_exception
+
+    logger.info(f"buyer with id {buyer_id} validated successfully")
+    return buyer
