e783d59b1716be16147762506225182b2b6b2510
salarKaf <salarikosar5@gmail.com>
2025-07-26 13:58:58 +0330

    refactor(getNewestItems): remove Authorization header from newest items API call

--- src/API/Items.jsx
+++ src/API/Items.jsx
@@ -2,13 +2,11 @@ import axios from "axios";
 import { coreBaseURL, mediaBaseURL } from './api';
 
 export const getNewestItems = async (websiteId, limit = 3) => {
-  const token = localStorage.getItem("token");
   try {
     const response = await axios.get(
       `${coreBaseURL}/items/newest_items?website_id=${websiteId}&limit=${limit}`,
       {
         headers: {
-          Authorization: `Bearer ${token}`,
         },
       }
     );
